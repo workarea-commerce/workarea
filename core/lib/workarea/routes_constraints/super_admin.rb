730b1f7dc0a98787669e1d50f790ad3fbdf84bc9
Matt Duffy <mduffy@workarea.com>
2019-10-01 09:28:55 -0400

    Check session for user_id over cookies (#131)
    The RoutingConstraints::SuperAdmin protects access to the Sidekiq
    web interface. The constraint needed to be updated to reflect the
    storing of user_id in session as of v3.5
    
    No changelog

--- core/lib/workarea/routes_constraints/super_admin.rb
+++ core/lib/workarea/routes_constraints/super_admin.rb
@@ -2,7 +2,7 @@ module Workarea
   module RoutesConstraints
     class SuperAdmin
       def matches?(request)
-        user_id = request.cookie_jar.signed[:user_id]
+        user_id = request.session[:user_id]
         return false if user_id.blank?
         User.find(user_id).super_admin?
       end
