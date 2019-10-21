20cc450ddd5edf16914a09bd740ed59b58bfc76e
Ben Crouse <bcrouse@workarea.com>
2019-10-21 14:14:32 -0400

    Don't get too fancy with first time visitor rules
    This can cause confusion/inconsistency with how other things (e.g.
    logout) affect a "visit".
    
    No changelog

--- core/app/models/workarea/segment/first_time_visitor.rb
+++ core/app/models/workarea/segment/first_time_visitor.rb
@@ -2,11 +2,7 @@ module Workarea
   class Segment
     class FirstTimeVisitor < Segment
       include LifeCycle
-
-      self.default_rules = [
-        Rules::Sessions.new(minimum: 0, maximum: 1),
-        Rules::Orders.new(maximum: 0)
-      ]
+      self.default_rules = [Rules::Sessions.new(minimum: 0, maximum: 1)]
     end
   end
 end
