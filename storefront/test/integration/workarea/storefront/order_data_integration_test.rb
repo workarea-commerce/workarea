ffd52a196dce93a8542afd768327f7e452ea5110
Tom Scott <tscott@weblinc.com>
2019-11-18 16:47:31 -0500

    Update Tests Referencing 2020
    The credit card expiration year `2020` was hard-coded into many Workarea
    integration tests, and would fail when January 2020 passes. Update these
    tests to always set the credit card expiration year to 1 year in
    advance of when the test runs so this won't happen again in the future.
    
    WORKAREA-104
    
    Fixes #222

--- storefront/test/integration/workarea/storefront/order_data_integration_test.rb
+++ storefront/test/integration/workarea/storefront/order_data_integration_test.rb
@@ -108,7 +108,7 @@ module Workarea
         assert_equal('Test Card', payment.credit_card.issuer)
         assert_equal('XXXX-XXXX-XXXX-1', payment.credit_card.display_number)
         assert_equal(1, payment.credit_card.month)
-        assert_equal(2020, payment.credit_card.year)
+        assert_equal(next_year, payment.credit_card.year)
         refute(payment.credit_card.token.blank?)
         assert(payment.credit_card.saved_card_id.blank?)
 
