2f0f923488caf9630efb03665f40d8b424d7ea65
Curt Howard <choward@workarea.com>
2020-01-17 09:57:05 -0800

    Ignore elements with no ID value when announcing duplicate IDs on-page
    WORKAREA.duplicateID was throwing a false positive exception when it
    would find elements containing an `id` attribute with no value
    specified. This behavior should be allowed, since empty ID values should
    pose no issues for the developer
    
    WORKAREA-184

--- core/test/javascripts/duplicate_id_spec.js
+++ core/test/javascripts/duplicate_id_spec.js
@@ -1,4 +1,5 @@
 //= require workarea/core/modules/environment
+//= require workarea/core/modules/string
 //= require workarea/core/modules/duplicate_id
 
 (function () {
@@ -7,8 +8,13 @@
     describe('WORKAREA.duplicateId', function () {
         describe('init', function () {
             it('throws error on duplicate ids', function () {
-                this.fixtures = fixture.load('duplicate_id.html');
+                this.fixtures = fixture.load('duplicate_id_fail.html');
                 expect(WORKAREA.duplicateId.init).to.throw(Error, /foo|bar/);
+            });
+
+            it('should allow for empty ID values', function () {
+                this.fixtures = fixture.load('duplicate_id_pass.html');
+                expect(WORKAREA.duplicateId.init).to.not.throw(Error);
             });
         });
     });
