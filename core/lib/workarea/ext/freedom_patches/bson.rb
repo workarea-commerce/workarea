7f639ebc236c99bd50f19ccdf89c8f12c9c9c5c0
Tom Scott <tscott@weblinc.com>
2020-03-02 14:39:39 -0500

    Fix BSON Bug
    Return the `BSON::Time#to_bson` method back to its original
    implementation to solve the failures in metrics, reporting, and insights
    tests.
    
    WORKAREA-217

--- core/lib/workarea/ext/freedom_patches/bson.rb
+++ core/lib/workarea/ext/freedom_patches/bson.rb
@@ -8,4 +8,10 @@ module BSON
       to_s
     end
   end
+
+  module Time
+    def to_bson(buffer = ByteBuffer.new, validating_keys = Config.validating_keys?)
+      buffer.put_int64((to_i * 1000) + (usec / 1000))
+    end
+  end
 end
