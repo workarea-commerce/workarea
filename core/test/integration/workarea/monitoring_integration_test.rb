2411b317fe235270870546920cad55973e9d2b71
Eric Pigeon <epigeon@workarea.com>
2020-03-23 15:09:37 -0400

    Update critical easymon checks
    Only elasticsearch, mongodb and redis are critical services for running
    the application.

--- core/test/integration/workarea/monitoring_integration_test.rb
+++ core/test/integration/workarea/monitoring_integration_test.rb
@@ -3,27 +3,32 @@ require 'test_helper'
 module Workarea
   class MonitoringIntegrationTest < Workarea::IntegrationTest
     def test_monitors_the_mongodb_status
-      get workarea.easymon_path('mongodb')
+      get workarea.easymon_path + '/mongodb'
       assert_includes(response.body, 'Up')
     end
 
     def test_monitors_the_redis_status
-      get workarea.easymon_path('redis')
+      get workarea.easymon_path + '/redis'
       assert_includes(response.body, 'Up')
     end
 
     def test_monitors_the_elasticsearch_status
-      get workarea.easymon_path('elasticsearch')
+      get workarea.easymon_path + '/elasticsearch'
       assert_includes(response.body, 'Up')
     end
 
     def test_monitors_the_sidekiq_queue_status
-      get workarea.easymon_path('sidekiq-queue')
+      get workarea.easymon_path + '/sidekiq-queue'
       assert_includes(response.body, 'Low')
     end
 
     def test_monitors_for_load_balancing
-      get workarea.easymon_path('load-balancing')
+      get workarea.easymon_path + '/load-balancing'
+      assert_includes(response.body, 'Up')
+    end
+
+    def test_critical_endpoint
+      get workarea.easymon_path + "/critical"
       assert_includes(response.body, 'Up')
     end
   end
