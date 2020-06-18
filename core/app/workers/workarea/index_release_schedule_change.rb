be8a51e7923960cbcf0fb2dd2d1b9abfd9e991a7
Ben Crouse <bcrouse@workarea.com>
2020-06-18 10:19:52 -0400

    Fix index duplicates after a release is removed
    When a release is deleted, its changes must be reindexed to fix previews
    for releases scheduled after it. This manifests as duplicate products
    when previewing releases.

--- core/app/workers/workarea/index_release_schedule_change.rb
+++ core/app/workers/workarea/index_release_schedule_change.rb
@@ -1,20 +1,22 @@
 module Workarea
-  class ReindexRelease
+  class IndexReleaseScheduleChange
     include Sidekiq::Worker
     include Sidekiq::CallbacksWorker
 
     sidekiq_options(
       enqueue_on: {
-        Release => :save,
-        only_if: -> { publish_at_changed? },
+        Release => [:save, :destroy],
+        only_if: -> { publish_at_changed? || destroyed? },
         with: -> { [id, publish_at_was, publish_at] }
       },
       queue: 'releases'
     )
 
     def perform(id, previous_publish_at, new_publish_at)
-      rescheduled_release = Release.find(id)
-      earlier, later = if previous_publish_at.present? && new_publish_at.present?
+      # When destroyed, changesets for the release ID will still exist and be used to update the index
+      rescheduled_release = Release.find_or_initialize_by(id: id)
+
+      earlier, later = if rescheduled_release.persisted? && previous_publish_at.present? && new_publish_at.present?
         [previous_publish_at, new_publish_at].sort
       elsif previous_publish_at.present?
         [previous_publish_at, nil]
