ee9793fd572ae2e758c1585ded7d50ad5ccee8b0
Tom Scott <tscott@weblinc.com>
2020-07-02 18:17:55 -0400

    Use legacy JS gem for testing and initial rewrites

--- storefront/test/dummy/config/initializers/workarea.rb
+++ storefront/test/dummy/config/initializers/workarea.rb
@@ -5,4 +5,15 @@ Workarea.configure do |config|
     root_path: '/tmp/workarea_storefront',
     server_root: '/tmp/workarea_storefront'
   }
+
+  # Disable legacy modules that have been rewritten in base so tests
+  # will be run on the rewritten code
+  config.legacy_javascript.storefront.modules.delete('workarea/core/modules/form_submitting_controls')
+  config.legacy_javascript.storefront.modules.delete('workarea/storefront/modules/messages')
+  config.legacy_javascript.storefront.modules.delete('workarea/storefront/modules/dialog')
+  config.legacy_javascript.storefront.modules.delete('workarea/storefront/modules/dialog_buttons')
+  config.legacy_javascript.storefront.modules.delete('workarea/storefront/modules/dialog_forms')
+  config.legacy_javascript.storefront.modules.delete('workarea/storefront/modules/dialog_close_buttons')
+  config.legacy_javascript.storefront.modules.delete('workarea/storefront/modules/primary_nav_content')
+  config.legacy_javascript.storefront.modules.delete('workarea/storefront/modules/admin_toolbar')
 end
