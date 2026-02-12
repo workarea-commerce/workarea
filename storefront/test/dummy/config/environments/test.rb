47f12ee0211b426ee8bb7ce689bb5530ebcc7cf5
Jason Hill <jhill@macmini.tail999711.ts.net>
2026-02-11 22:49:49 -0500

    WA-010 Replace deprecated raise_on_missing_translations config

--- storefront/test/dummy/config/environments/test.rb
+++ storefront/test/dummy/config/environments/test.rb
@@ -39,5 +39,5 @@ Rails.application.configure do
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations
-  config.action_view.raise_on_missing_translations = true
+  config.i18n.raise_on_missing_translations = true
 end
