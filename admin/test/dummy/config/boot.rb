18db6d6f0dfeb0e9dba505a92de3bd7a1b960077
Jason Hill <jhill@macmini.tail999711.ts.net>
2026-02-12 23:44:47 -0500

    WA-018: Verify ES 7.x compatibility in storefront/admin + fix admin Logger boot
    No ES 7.x mapping type changes required in storefront/ or admin/.
    All Elasticsearch code is centralized in core (fixed in WA-017).
    
    Incidental fix: admin/test/dummy/config/boot.rb Logger boot order
    - Fixes 'uninitialized constant ...::Logger' error in Ruby 2.6+ Rails 6.1
    - Matches storefront fix from commit 117cff49

--- admin/test/dummy/config/boot.rb
+++ admin/test/dummy/config/boot.rb
@@ -1,3 +1,10 @@
+# Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../../../Gemfile', __dir__)
 
-require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
+# Ruby 2.6+ + Rails 6.1 can hit `uninitialized constant ...::Logger` if stdlib
+# Logger isn't loaded before ActiveSupport initializes.
+require 'logger'
+
+$:.unshift File.expand_path('../../../../lib', __dir__)
